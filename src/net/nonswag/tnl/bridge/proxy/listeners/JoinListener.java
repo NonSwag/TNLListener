5f265364c17906fd98fe064dce4538e487652949
NonSwag <MrMinecraft00@gmail.com>
2021-02-08 10:59:12 +0100

    Update 2.2

--- src/net/nonswag/tnl/bridge/proxy/listeners/JoinListener.java
+++ src/net/nonswag/tnl/bridge/proxy/listeners/JoinListener.java
@@ -3,7 +3,6 @@ package net.nonswag.tnl.bridge.proxy.listeners;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.ServerPostConnectEvent;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import net.kyori.adventure.text.Component;
 import net.nonswag.tnl.bridge.Packet;
 import net.nonswag.tnl.bridge.PacketListener;
 import net.nonswag.tnl.bridge.packets.PlayerInfoPacket;
@@ -27,13 +26,7 @@ public class JoinListener {
                 List<Packet<PacketListener>> packets = new ArrayList<>();
                 packets.add(new PlayerInfoPacket(event.getPlayer().getUniqueId(), new Set<>("version", event.getPlayer().getProtocolVersion().getProtocol())));
                 PacketHandler.sendPackets(connectedServer, packets);
-            } else {
-                event.getPlayer().disconnect(Component.text(Bridge.getPrefix() + "\n" +
-                        "§cThe Server §4" + server.getServerInfo().getName() + "§c didn't answered"));
             }
-        } else {
-            event.getPlayer().disconnect(Component.text(Bridge.getPrefix() + "\n" +
-                    "§cThe Server didn't answered"));
         }
     }
 }
